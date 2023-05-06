18f4f31c97ce57ecb395d503e2002654b1f5fc89
Smark <ynlbq@outlook.com>
2023-05-06 15:31:33 +0800

    style: 修改上传文件样式

--- web/src/pages/upload/index.js
+++ web/src/pages/upload/index.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const Upload = () => {
   const [file, setFile] = useState(null);
   const [filename, setFilename] = useState('');
+  const [uploadSuccess, setUploadSuccess] = useState(false);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -21,20 +22,25 @@ const Upload = () => {
       });
       const result = await response.json();
       console.log(result);
+      setUploadSuccess(true);
     } catch (error) {
       console.error(error);
+      setUploadSuccess(false);
     }
   };
 
   return (
-    <div>
-      <h1>上传文件</h1>
-      <div>
-        <input type="file" onChange={handleFileChange} />
-        <button onClick={handleUpload}>上传</button>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <h1 style={{marginBottom: '20px'}}>上传文件</h1>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <input type="file" onChange={handleFileChange} style={{marginBottom: '20px'}} />
+        <button onClick={handleUpload} style={{padding: '10px 20px', backgroundColor: '#4CAF50', color: 'white', border: 'none', borderRadius: '5px'}}>上传</button>
+        {uploadSuccess && <p style={{color: 'green'}}>上传成功</p>}
+        {!uploadSuccess && <p style={{color: 'red'}}>未上传文件/上传失败</p>}
       </div>
     </div>
   );
 };
 
 export default Upload;
+
